fd61dcf3aa4609205bd8e0984b4cf7c75fff578b
Xdman509 <xdman509@gmail.com>
2023-11-13 17:35:18 -0600

    patient data

---
+++ models/patients.js
@@ -0,0 +1,69 @@
+const mongoose = require('mongoose');
+
+// Create Schema
+const patientSchema = new mongoose.Schema(
+    {
+      creatorId: {
+        type: String,
+        required: true,
+      },
+      creatorName: {
+        type: String,
+        required: true,
+      },
+      firstName: {
+        type: String,
+        required: true,
+      },
+      lastName: {
+        type: String,
+        required: true,
+      },
+      birthdate: {
+        type: Date,
+        required: true,
+      },
+      zipcode: {
+        type: String,
+        required: true,
+      },
+      state: {
+        type: String,
+        required: true,
+      },
+      phoneNumber: {
+        type: String,
+        required: true,
+      },
+      createDate: {
+        type: Date,
+        required: true,
+      },
+      insuranceType: {
+        type: String,
+        required: true,
+      },
+      testType: {
+        type: String,
+        required: true,
+      },
+      doctorService: {
+        type: String,
+        required: true,
+      },
+      labName: {
+        type: String,
+        required: true,
+      },
+      sampleStatus: {
+        type: String,
+        required: true,
+      },
+    },
+    {strict: false},
+);
+
+
+const Patient = mongoose.model('Patient', patientSchema);
+
+module.exports = Patient;
--- routes/patients.js
+++ routes/patients.js
@@ -1,14 +1,84 @@
 const express = require('express');
 const router = express.Router();
 const authMiddleware = require('../middleware/auth');
+const Patient = require('../models/patients');
 
+router.get('/', async function(req, res, next) {
+  const patients = await Patient.find();
+  res.render('patients/details', {patientData: patients});
+});
 
 router.get('/create', authMiddleware.ensureAuthenticated, function(req, res, next) {
   res.render('patients/create');
 });
 
-router.get('/details', authMiddleware.ensureAuthenticated, function(req, res, next) {
-  res.render('patients/details');
+router.post('/create', async function(req, res, next) {
+  const newPatient = new Patient(
+      {
+        creatorId: req.body.creatorId,
+        creatorName: req.body.creatorName,
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+        birthdate: req.body.birthdate,
+        zipcode: req.body.zipcode,
+        state: req.body.state,
+        phoneNumber: req.body.phoneNumber,
+        createDate: req.body.createDate,
+        insuranceType: req.body.insuranceType,
+        testType: req.body.testType,
+        doctorService: req.body.doctorService,
+        labName: req.body.labName,
+        sampleStatus: req.body.sampleStatus,
+      },
+  );
+
+  try {
+    await newPatient.save();
+  } catch (err) {
+    console.log(err);
+  }
+
+  res.redirect('/patients');
+});
+
+router.get('/update', async function(req, res, next) {
+  const id = req.query._id;
+
+  const patient = await Patient.findById(id);
+
+  res.render('patients/edit', {patientData: patient});
+});
+
+router.post('/update', async function(req, res, next) {
+  const id = req.body._id;
+
+  await Patient.findOneAndUpdate({_id: id}, {
+    creatorId: req.body.creatorId,
+    creatorName: req.body.creatorName,
+    firstName: req.body.firstName,
+    lastName: req.body.lastName,
+    birthdate: req.body.birthdate,
+    zipcode: req.body.zipcode,
+    state: req.body.state,
+    phoneNumber: req.body.phoneNumber,
+    createDate: req.body.createDate,
+    insuranceType: req.body.insuranceType,
+    testType: req.body.testType,
+    doctorService: req.body.doctorService,
+    labName: req.body.labName,
+    sampleStatus: req.body.sampleStatus,
+  });
+
+
+  res.redirect('/patients');
+});
+
+router.get('/delete', async function(req, res, next) {
+  const id = req.query._id;
+
+  await Patient.findByIdAndDelete(id);
+
+  res.redirect('/patients');
 });
 
 module.exports = router;
